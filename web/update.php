b5a435b347a8348f9c229fba53b61dbb2c7070ed
lvlurilo <murilocarvalho.br@hotmail.com>
2022-01-14 22:03:32 -0300

    inicio estilo css

--- web/update.php
+++ web/update.php
@@ -1,33 +1,34 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="">
-    <title>Alterar album</title>
+<link rel="stylesheet" type="text/css" href="">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+    <title>Novo album</title>
 </head>
 <body>
-    <header>
-        <a href="../index.html"><button type="submit">HOME</button></a>
-        <a href="read.php"><button type="submit">LISTAR ALBUNS</button></a>
+    <header class="header">
+            <a href="../index.html">HOME</a>
+            <a href="read.php">LISTAR ALBUNS</a>
     </header>
-
-    <section id="update">   
+    <br>
+    <section class="formulario">   
         <form action="setupdate.php" method="POST" enctype="multipart/form-data">
             <input type="hidden" name="id" value="<?php echo $_POST['id']?>">
-            <label>Titulo</label>
-            <input type="text" id="titulo "name="titulo" value="<?php echo $_POST['titulo'] ?>" required><br>
-            <label>Banda</label>
-            <input type="text" id="banda" name="banda" value="<?php echo $_POST['banda'] ?>" required><br>
-            <label>Ano</label>
-            <input type="text" id="ano "name="ano" minlength="4" maxlength="4" value="<?php echo $_POST['ano'] ?>" required><br>
             <label>Capa Atual</label>
             <input type="hidden" name="capa" id="capa" value= "<?php echo $_POST['capa']?>">
             <img src= "../img/<?php echo $_POST['capa']?>"><br>
-            <label>Nova Capa</label>
-            <input type="file" id="newcapa" name="newcapa"><br>
-            <button type="submit" id="buttonEditar" name="buttonEditar">EDITAR</button>
+            <label></label>
+            <input type="file" class="capa" name="newcapa"><br>
+            <label></label>
+            <input type="text" class="ipt" name="titulo" value="<?php echo $_POST['titulo'] ?>" required><br>
+            <label></label>
+            <input type="text" class="ipt" name="banda" value="<?php echo $_POST['banda'] ?>" required><br>
+            <label></label>
+            <input type="text" class="ipt" name="ano" minlength="4" maxlength="4" value="<?php echo $_POST['ano'] ?>" required><br>
+            
+            <button type="submit" class="btn" name="buttonEditar">EDITAR</button>
         </form>
     </section>
 
