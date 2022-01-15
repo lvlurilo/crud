b0eac0f49d4d8477ecb6daee85cb92d221970073
lvlurilo <murilocarvalho.br@hotmail.com>
2022-01-15 17:09:08 -0300

    css - home create update

--- web/read.php
+++ web/read.php
@@ -5,48 +5,58 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>albuns preferidos</title>
+    <link rel="stylesheet" type="text/css" href="../css/read.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+    <title>Novo album</title>
 </head>
 <body>
-<header>
-    <a href="../index.html"><button type="submit">HOME</button></a>
-    <a href="create.html"><button type="submit">CADASTRAR NOVO ALBUM</button></a> 
-</header>
-<body>
-    
-<?php 
-    require_once('../lib/conexao.php');
-    require_once('../lib/album.php');
-    require_once('../lib/crud.php');
+    <header>
+            <a href="../index.html">HOME</a>
+            <a href="create.html">CADASTRAR NOVO ALBUM</a>
+    </header>
+    <section>   
+    <?php 
+        require_once('../lib/conexao.php');
+        require_once('../lib/album.php');
+        require_once('../lib/crud.php');
 
-    $crud = new crud();
-    $crud->read();
+        $crud = new crud();
+        $crud->read();
 
-    foreach($crud->read() as $album){?>
-
-        <img src= "../img/<?php echo $album['capa']?>" alt="<?php $album['titulo']?>"><br><?php
-
-        echo $album['titulo']."<br>".$album['banda']."<br>".$album['ano']."<br>";
-        $album['id'];
-        ?>
-
-        <form action="update.php" method="POST">
-            <input type="hidden" name="id" value="<?php echo $album['id']?>">
-            <input type="hidden" name="titulo" value="<?php echo $album['titulo']?>">
-            <input type="hidden" name="banda" value="<?php echo $album['banda']?>">
-            <input type="hidden" name="ano" value="<?php echo $album['ano']?>">
-            <input type="hidden" name="capa" value="<?php echo $album['capa']?>">
-            <button type="submit" id="editar">EDITAR</button>
-        </form>
-        
-        <form action="delete.php" method="GET">
-            <input type="hidden" name="id" value="<?php echo $album['id']?>">
-            <input type="hidden" name="capa" value="<?php echo $album['capa']?>">
-            <button type="submit" id="excluir">EXCLUIR</button>
-        </form>
-        <hr>
-    <?php
-    }?>
-    
+        foreach($crud->read() as $album){?>
+            <div class="capa">
+                <img src= "../img/<?php echo $album['capa']?>" alt="<?php $album['titulo']?>"><br>
+            <div>
+            <div class="fomrulario">
+                <div class="info">
+                    <?php
+                    echo $album['titulo']."<br>".$album['banda']."<br>".$album['ano']."<br>";
+                    $album['id'];
+                    ?>
+                </div>
+                <form action="update.php" method="POST">
+                    <input type="hidden" name="id" value="<?php echo $album['id']?>">
+                    <input type="hidden" name="titulo" value="<?php echo $album['titulo']?>">
+                    <input type="hidden" name="banda" value="<?php echo $album['banda']?>">
+                    <input type="hidden" name="ano" value="<?php echo $album['ano']?>">
+                    <input type="hidden" name="capa" value="<?php echo $album['capa']?>">
+                    <button type="submit" class="btn" id="editar">EDITAR</button>
+                </form>
+                <br>
+                <form action="delete.php" method="GET">
+                    <input type="hidden" name="id" value="<?php echo $album['id']?>">
+                    <input type="hidden" name="capa" value="<?php echo $album['capa']?>">
+                    <button type="submit" class="btn" id="excluir">EXCLUIR</button>
+                </form>
+                </div>
+        <?php
+        }?>
+           
+    </section>
+    <footer>
+        <p>github.com/lvlurilo</p>
+    </footer> 
 </body>
 </html>
--- web/update.php
+++ web/update.php
@@ -1,40 +1,40 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
-<link rel="stylesheet" type="text/css" href="">
+<link rel="stylesheet" type="text/css" href="../css/create-update.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
     <title>Novo album</title>
 </head>
 <body>
-    <header class="header">
+    <header>
             <a href="../index.html">HOME</a>
             <a href="read.php">LISTAR ALBUNS</a>
     </header>
-    <br>
-    <section class="formulario">   
+    <section>   
         <form action="setupdate.php" method="POST" enctype="multipart/form-data">
-            <input type="hidden" name="id" value="<?php echo $_POST['id']?>">
-            <label>Capa Atual</label>
-            <input type="hidden" name="capa" id="capa" value= "<?php echo $_POST['capa']?>">
-            <img src= "../img/<?php echo $_POST['capa']?>"><br>
-            <label></label>
-            <input type="file" class="capa" name="newcapa"><br>
-            <label></label>
-            <input type="text" class="ipt" name="titulo" value="<?php echo $_POST['titulo'] ?>" required><br>
-            <label></label>
-            <input type="text" class="ipt" name="banda" value="<?php echo $_POST['banda'] ?>" required><br>
-            <label></label>
-            <input type="text" class="ipt" name="ano" minlength="4" maxlength="4" value="<?php echo $_POST['ano'] ?>" required><br>
-            
-            <button type="submit" class="btn" name="buttonEditar">EDITAR</button>
+            <div class = "capa">
+                <img src= "../img/<?php echo $_POST['capa']?>">
+            </div>
+            <div class= "formulario">
+                <input type="hidden" name="id" value="<?php echo $_POST['id']?>">
+                <label></label>
+                <input type="hidden" name="capa" id="capa" value= "<?php echo $_POST['capa']?>">
+                <label></label>
+                <input type="file" class="ipt_img" name="newcapa"><br>
+                <label></label>
+                <input type="text" class="ipt" name="titulo" value="<?php echo $_POST['titulo'] ?>" required><br>
+                <label></label>
+                <input type="text" class="ipt" name="banda" value="<?php echo $_POST['banda'] ?>" required><br>
+                <label></label>
+                <input type="text" class="ipt" name="ano" minlength="4" maxlength="4" value="<?php echo $_POST['ano'] ?>" required><br>
+                <button type="submit" class="btn" name="buttonEditar">EDITAR</button>
+            <div>
         </form>
     </section>
-
     <footer>
-
-
+        <p>github.com/lvlurilo</p>
     </footer> 
 </body>
 </html>
