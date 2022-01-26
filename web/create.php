5ca94c7644678b326f5335c726e918a5d6d63984
Murilo Carvalho <murilocarvalho.br@hotmail.com>
2022-01-25 22:11:45 -0300

    revisao botao arquivo

--- web/create.php
+++ web/create.php
@@ -15,17 +15,15 @@
             <a href="../index.html">HOME</a>
             <a href="read.php">LISTAR ALBUNS</a>
     </header>
-    <section> 
+     
+    <div class="main">
+        <section>
         <form action="setcreate.php" method="POST" enctype="multipart/form-data">
-            <!-- <div class = "capa">
-                <img>
-            </div> -->
             <div class= "formulario">
                 <label></label>
-                <!-- <input type="file" class="ipt_img" name="capa" accept="image/*"><br> -->
-                <input type="button" class="btn_file" value="Escolha uma imagem de capa" onclick="document.getElementById('capa').click();"/>
-                <input type="file" style="display:none;" id="capa" name="capa" accept="image/*" required><br>
-                <span></span>
+                <input type="file"  id="capa" name="capa" accept="image/*" style="display:none;" required><br>
+                <button type="button" id="capabt" name="btncapa" class="btn" >Selecione uma capa</button><br>
+                <span id="endereco" name="endereco">Formatos: .jpeg .jpg .png</span><br>
                 <label></label>
                 <input type="text" class="ipt "name="titulo" placeholder="Titulo Album" required><br>
                 <label></label>
@@ -35,7 +33,28 @@
                 <button type="submit" class="btn" name="buttonCadastrar">CADASTRAR</button>
             </div>
         </form>
-    </section>
+        </section>
+    </div>
+    
+    <script type="text/javascript">
+        const capa = document.getElementById("capa")
+        const btncapa = document.getElementById("capabt")
+        const span = document.getElementById("endereco")
+
+        btncapa.addEventListener("click", function() {
+            capa.click()
+        })
+
+        capa.addEventListener("change", function() {
+            if(capa.value){
+                span.innerHTML = capa.value.match(/[\/\\]([\w\d\s\.\-\(\)]+)$/)[1]
+            }else{
+                span.innerHTML = "Formatos: .jpeg .jpg .png" 
+            }
+        })
+
+
+    </script>
     <footer>
         <p>github.com/lvlurilo</p>
     </footer> 
